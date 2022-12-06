fe977223f8a061e6818aa25890d67965db268302
Hartz2 <80923471+Hartz2@users.noreply.github.com>
2022-12-06 14:52:36 -0700

    Correcting your tables and adding a stored procedure

--- distribution.sql
+++ distribution.sql
@@ -11,7 +11,7 @@ CREATE TABLE dis_manager(manager_id INT(5), runner_id INT(5));
 -- quantity: amount of product ordered
 -- product_type: type of product
 
-CREATE TABLE dis_runner_out(runner_id INT(5), date_out DATETIME, method VARCHAR(20), quantity INT(2), product_type VARCHAR(20));
+CREATE TABLE dis_runner_out(runner_id INT(5), date_out DATETIME, method VARCHAR(20), quantity INT(2), product_type VARCHAR(20), market_value INT(3));
 
 -- creates table: dis_runner_in
 -- runner_id: unique ID of runner
@@ -31,11 +31,11 @@ INSERT INTO dis_manager VALUES ('30005', '40005');
 
 -- insert data into dis_runner_out table 
 
-INSERT INTO dis_runner_out VALUES ('40001', '2001-01-28 00:00:00', 'car', '2', 'product type');
-INSERT INTO dis_runner_out VALUES ('40002', '2022-12-01 02:00:00', 'car', '9', 'product type');
-INSERT INTO dis_runner_out VALUES ('40003', '2005-03-31 07:00:00', 'plane', '14', 'product type');
-INSERT INTO dis_runner_out VALUES ('40004', '2018-06-19 12:00:00', 'plane', '22', 'product type');
-INSERT INTO dis_runner_out VALUES ('40005', '2011-02-11 19:00:00', 'car', '5', 'product type');
+INSERT INTO dis_runner_out VALUES (40001, '2001-01-28 00:00:00', 'car', '2', 'Cannabis', 50);
+INSERT INTO dis_runner_out VALUES (40002, '2022-12-01 02:00:00', 'car', '9', 'Heroin', 150);
+INSERT INTO dis_runner_out VALUES (40003, '2005-03-31 07:00:00', 'plane', '14', 'LSD Acid', 200);
+INSERT INTO dis_runner_out VALUES (40004, '2018-06-19 12:00:00', 'plane', '22', 'PCP', 40);
+INSERT INTO dis_runner_out VALUES (40005, '2011-02-11 19:00:00', 'car', '5', 'MDMA', 70);
 
 -- insert data into dis_runner_in table
 
@@ -48,3 +48,11 @@ INSERT INTO dis_runner_in VALUES ('40005', '2011-02-11 22:00:00', '600', '0');
 -- Manager table request
 SELECT dis_runner_in.runner_id, date_out, date_in, method, product_type, collected, quantity 
 FROM dis_runner_in JOIN dis_runner_out ON dis_runner_out.runner_id = dis_runner_in.runner_id;
+
+-- Runner out
+DELIMITER $$
+CREATE PROCEDURE runner_out(id INT(5))
+BEGIN
+SELECT quantity, product_type, method, quantity*market_value AS collect 
+FROM dis_runner_out WHERE id = runner_id;
+END $$
